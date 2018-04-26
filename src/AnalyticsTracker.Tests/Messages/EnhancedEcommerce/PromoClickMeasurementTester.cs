946b6d6b7b30396927aaaad4b90b645d56ab87b2
mzanoni <zanoniii@gmail.com>
2018-04-26 10:13:03 +0200

    Added promo measurements

---
+++ src/AnalyticsTracker.Tests/Messages/EnhancedEcommerce/PromoClickMeasurementTester.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Vertica.AnalyticsTracker.Commands.EnhancedEcommerce.FieldObjects;
+using Vertica.AnalyticsTracker.Messages.EnhancedEcommerce;
+
+namespace AnalyticsTracker.Tests.Messages.EnhancedEcommerce
+{
+    [TestFixture]
+    public class PromoClickMeasurementTester
+    {
+        [Test]
+        public void RenderMessage_AddsPromoView()
+        {
+            var subj = new PromoClickMeasurement(new[] { new PromoFieldObject("JUNE_PROMO13", "June Sale") });
+            var renderedMessage = subj.RenderMessage();
+            Assert.That(renderedMessage, Is.StringContaining("{'event': 'promotionClick','ecommerce': {'promoClick': {'promotions': [{'id': 'JUNE_PROMO13','name': 'June Sale'}]}}}"));
+        }
+
+        [Test]
+        public void RenderMessage_AddsPromoViewWithOptional()
+        {
+            var subj = new PromoClickMeasurement(new[] { new PromoFieldObject("JUNE_PROMO13", "June Sale", creative: "banner1", position: "slot1") });
+            var renderedMessage = subj.RenderMessage();
+            Assert.That(renderedMessage, Is.StringContaining("{'event': 'promotionClick','ecommerce': {'promoClick': {'promotions': [{'id': 'JUNE_PROMO13','name': 'June Sale','creative': 'banner1','position': 'slot1'}]}}}"));
+        }
+    }
+}
---
+++ src/AnalyticsTracker/Messages/EnhancedEcommerce/PromoClickMeasurement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Vertica.AnalyticsTracker.Commands.EnhancedEcommerce.FieldObjects;
+
+namespace Vertica.AnalyticsTracker.Messages.EnhancedEcommerce
+{
+    public class PromoClickMeasurement : EnhancedEcommerceMeasurementBase
+    {
+        private readonly PromoFieldObject[] _promoFieldObjects;
+
+        public PromoClickMeasurement(PromoFieldObject[] promoFieldObjects) : base("promotionClick")
+        {
+            _promoFieldObjects = promoFieldObjects;
+        }
+
+        public override Dictionary<string, object> CreateMeasurement()
+        {
+            var promotions =
+                new Dictionary<string, object> { { "promotions", _promoFieldObjects.Select(i => i.Info).ToArray() } };
+
+            return new Dictionary<string, object>
+            {
+                { "promoClick", promotions }
+            };
+        }
+    }
+}
